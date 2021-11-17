7b012b1ee58233f15d05ccdcbaeb421fd6154187
HaiAnh <haianhnguyen12082001>
2021-11-17 10:27:28 +0700

    first commit

---
+++ .history/assets/js/main_20211116171702.js
@@ -0,0 +1,24 @@
+$(function () {
+    $(".photoLibrary__list").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        prevArrow: ('<div class="slick-prev"><i class="fas fa-chevron-left"></i></div>'),
+        nextArrow: ('<div class="slick-next"><i class="fas fa-chevron-right"></i></div>'),
+    });
+
+    ///Feedback
+    // $(".feedback__list").slick({
+    //     infinite: true,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     arrows: true,
+    //     autoplay: true,
+    //     speed: 2000,
+    //     autoplaySpeed: 3000,
+    // });
+})
